3d5a3057618341322e8158f5256bef30592d1b63
Pedro Silva <pdx.henrique@gmail.com>
2023-10-26 03:09:34 -0300

    Declare typography tokens as contant

--- packages/ds/lib/src/tokens/typography.dart
+++ packages/ds/lib/src/tokens/typography.dart
@@ -4,19 +4,19 @@ import 'color.dart';
 import 'dimen.dart';
 
 class AppFontFamily {
-  static final highlight = 'Archivo';
+  static const highlight = 'Archivo';
 }
 
 class AppFontWeight {
-  static final bold = FontWeight.w700;
+  static const bold = FontWeight.w700;
 
-  static final medium = FontWeight.w600;
+  static const medium = FontWeight.w600;
 
-  static final regular = FontWeight.w400;
+  static const regular = FontWeight.w400;
 }
 
 class AppTextStyle {
-  static final headingSmall = TextStyle(
+  static const headingSmall = TextStyle(
     fontSize: AppFontSize.lg,
     height: AppLineHeight.distant,
     fontFamily: AppFontFamily.highlight,
@@ -24,7 +24,7 @@ class AppTextStyle {
     color: AppColor.neutral01,
   );
 
-  static final subtitleSmall = TextStyle(
+  static const subtitleSmall = TextStyle(
     fontSize: AppFontSize.md,
     height: AppLineHeight.medium,
     fontFamily: AppFontFamily.highlight,
@@ -32,7 +32,7 @@ class AppTextStyle {
     color: AppColor.neutral02,
   );
 
-  static final paragraph = TextStyle(
+  static const paragraph = TextStyle(
     fontSize: AppFontSize.xs,
     height: AppLineHeight.distant,
     fontFamily: AppFontFamily.highlight,
