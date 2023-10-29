d4760556c9637070216b3b017de0b49588b569cb
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-10-29 20:28:46 -0300

    Add PercentageBorderRadius class

---
+++ packages/ds/lib/src/types/radius.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/painting.dart';
+
+class PercentageBorderRadius extends BorderRadius {
+  const PercentageBorderRadius.all(Radius radius) : super.all(radius);
+
+  @override
+  RRect toRRect(Rect rect) {
+    final topLeft = Radius.elliptical(
+      rect.width * this.topLeft.x,
+      rect.height * this.topLeft.y,
+    );
+
+    final topRight = Radius.elliptical(
+      rect.width * this.topRight.x,
+      rect.height * this.topRight.y,
+    );
+
+    final bottomLeft = Radius.elliptical(
+      rect.width * this.bottomLeft.x,
+      rect.height * this.bottomLeft.y,
+    );
+
+    final bottomRight = Radius.elliptical(
+      rect.width * this.bottomRight.x,
+      rect.height * this.bottomRight.y,
+    );
+
+    return RRect.fromRectAndCorners(
+      rect,
+      topLeft: topLeft.clamp(minimum: Radius.zero),
+      topRight: topRight.clamp(minimum: Radius.zero),
+      bottomLeft: bottomLeft.clamp(minimum: Radius.zero),
+      bottomRight: bottomRight.clamp(minimum: Radius.zero),
+    );
+  }
+}
