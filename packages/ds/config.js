044ed37d4d995af1c6ed4cadb0e84beca2eac6b9
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-10-29 20:27:02 -0300

    Update style dictionary config

--- packages/ds/config.js
+++ packages/ds/config.js
@@ -1,91 +1,111 @@
 const StyleDictionary = require('style-dictionary-generator')
 
 module.exports = StyleDictionary.extend({
-    source: [
-        "properties/**/*.json"
-    ],
-    platforms: {
-        // flutter: {
-        //     transformGroup: "meiuca-flutter",
-        //     buildPath: "./lib/",
-        //     files: [
-        //         // {
-        //         //     destination: "theme.dart",
-        //         //     format: "flutter/theme.dart",
-        //         // },
-        //         {
-        //             destination: "dimensions.dart",
-        //             format: "flutter/dimensions.dart",
-        //             prefix: "app",
-        //             filter: {
-        //                 attributes: {
-        //                     category: "size",
-        //                     type: "border",
-        //                 }
-        //             },
-        //         },
-        //     ],
-        // }
-        "flutter-separate": {
-            "transformGroup": "flutter-separate",
-            "buildPath": "./lib/src/style_dictionary/",
-            "files": [
-                {
-                    "destination": "color.dart",
-                    "format": "flutter/class.dart",
-                    "className": "JColor",
-                    "type": "color",
-                    "filter": {
-                        "attributes": {
-                            "category": "color"
-                        }
-                    }
-                },
-                {
-                    "destination": "border.dart",
-                    "format": "flutter/class.dart",
-                    "className": "JBorder",
-                    "type": "float",
-                    "filter": {
-                        "attributes": {
-                            "type": "border"
-                        }
-                    }
-                },
-                {
-                    "destination": "opacity.dart",
-                    "format": "flutter/class.dart",
-                    "className": "JOpacity",
-                    "type": "float",
-                    "filter": {
-                        "attributes": {
-                            "type": "opacity"
-                        }
-                    }
-                },
-                {
-                    "destination": "spacing.dart",
-                    "format": "flutter/class.dart",
-                    "className": "JSpacing",
-                    "type": "float",
-                    "filter": {
-                        "attributes": {
-                            "type": "spacing"
-                        }
-                    }
-                },
-                {
-                    "destination": "font.dart",
-                    "format": "flutter/class.dart",
-                    "className": "JFont",
-                    "type": "float",
-                    "filter": {
-                        "attributes": {
-                            "type": "font"
-                        }
-                    }
-                }
-            ]
-        }
-    }
-});
+  source: [
+    'properties/**/*.json',
+  ],
+  platforms: {
+    flutter: {
+      transformGroup: 'meiuca-flutter',
+      buildPath: './build/style_dictionary/',
+      actions: ['flutter/aggregate'],
+      basePxFontSize: 1,
+      options: {
+        showFileHeader: false,
+        flutterAggregate: {
+          outputPath: './lib/src',
+          className: 'JDesignTokens',
+          additionalImports: [
+            "import 'package:ds/src/types.dart';",
+            "import 'package:flutter/painting.dart';",
+          ],
+        },
+      },
+      files: [
+        {
+          destination: 'color.dart',
+          format: 'flutter/class.dart',
+          className: 'Color',
+          filter: {
+            attributes: {
+              category: 'color',
+            },
+          },
+        },
+        {
+          destination: 'text_style.dart',
+          format: 'flutter/text_style.dart',
+          className: 'TextStyle',
+          filter: {
+            attributes: {
+              type: 'text',
+              category: 'style',
+            },
+          },
+        },
+        {
+          destination: 'spacing.dart',
+          format: 'flutter/class.dart',
+          className: 'Spacing',
+          filter: {
+            attributes: {
+              type: 'spacing',
+            },
+          },
+        },
+        {
+          destination: 'spacing_squish.dart',
+          format: 'flutter/class.dart',
+          className: 'SpacingSquish',
+          filter: {
+            attributes: {
+              type: 'spacing_squish',
+              subitem: 'stack',
+            },
+          },
+        },
+        {
+          destination: 'border.dart',
+          format: 'flutter/class.dart',
+          className: 'Border',
+          filter: {
+            attributes: {
+              type: 'border',
+            },
+          },
+        },
+        {
+          destination: 'radius.dart',
+          format: 'flutter/class.dart',
+          className: 'Radius',
+          filter: {
+            attributes: {
+              type: 'radius',
+            },
+          },
+        },
+        {
+          destination: 'opacity.dart',
+          format: 'flutter/class.dart',
+          className: 'Opacity',
+          filter: {
+            attributes: {
+              type: 'opacity',
+            },
+          },
+        },
+        {
+          destination: 'shadow.dart',
+          format: 'flutter/class.dart',
+          className: 'Shadow',
+          filter: {
+            attributes: {
+              type: 'shadow',
+              subitem: 'stack',
+            },
+          },
+        },
+      ],
+    },
+  },
+})
