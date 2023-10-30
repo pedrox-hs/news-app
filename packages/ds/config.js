110cd5ce2cb110406ea8e339ee6d98aa06f72e64
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-10-30 06:33:17 -0300

    Update style dictionary
    - add new tokens
    - update config

--- packages/ds/config.js
+++ packages/ds/config.js
@@ -1,43 +1,46 @@
 const StyleDictionary = require('style-dictionary-generator')
 
-// override 'size/flutter/radius' transform
-// because we need to support percentage radius
-// unfortunately, Flutter does not support it yet
-// issue: https://github.com/flutter/flutter/issues/135689
-StyleDictionary.registerTransform({
-  name: 'size/flutter/radius',
-  type: 'value',
-  matcher: ({ value, attributes: { type } }) =>
-    type === 'radius' && value.split(' ').length === 1,
-  transformer: token => {
-    let value = parseFloat(token.value, 10)
-    if (token.value.includes('%')) {
-      value = value / 100
-      return `PercentageBorderRadius.all(Radius.circular(${value.toFixed(2)}))`
-    }
-    return `BorderRadius.all(Radius.circular(${value.toFixed(2)}))`
-  },
-})
-
 module.exports = StyleDictionary.extend({
   source: [
     'properties/**/*.json',
   ],
+  transform: {
+    // override 'size/flutter/radius' transform
+    // because we need to support percentage radius
+    // unfortunately, Flutter does not support it yet
+    // issue: https://github.com/flutter/flutter/issues/135689
+    'size/flutter/radius': {
+      type: 'value',
+      matcher: ({ value, attributes: { type, category } }) =>
+        (type === 'radius' || category === 'radius') && value.match(/^\d+(px|%)?$/),
+      transformer: token => {
+        let value = parseFloat(token.value, 10)
+        if (token.value.includes('%')) {
+          value = value / 100
+          return `PercentageBorderRadius.all(Radius.circular(${value.toFixed(2)}))`
+        }
+        return `BorderRadius.all(Radius.circular(${value.toFixed(2)}))`
+      },
+    },
+
+    // override 'size/flutter/asset' transform
+    // to transform asset uri to LocalSvgAsset type
+    'asset/flutter/path': {
+      type: 'value',
+      matcher: ({ value }) => typeof value === 'string' && value.match(/^assets:\/\/.+\.svg$/),
+      transformer: ({ value }) => `LocalSvgAsset('packages/ds/${value.replace(/:\//, '')}')`,
+    },
+  },
   platforms: {
     flutter: {
       transformGroup: 'meiuca-flutter',
       buildPath: './build/style_dictionary/',
       actions: ['flutter/aggregate'],
-      basePxFontSize: 1,
       options: {
         showFileHeader: false,
         flutterAggregate: {
           outputPath: './lib/src',
           className: 'JDesignTokens',
-          additionalImports: [
-            "import 'package:ds/src/types.dart';",
-            "import 'package:flutter/painting.dart';",
-          ],
         },
       },
       files: [
@@ -52,17 +55,12 @@ module.exports = StyleDictionary.extend({
           },
         },
         {
-          destination: 'typography.dart',
-          format: 'flutter/factory.dart',
-          className: 'Typography',
-          factoryName: 'TextStyle',
-          imports: [
-            'package:flutter/painting.dart',
-          ],
+          destination: 'font.dart',
+          format: 'flutter/class.dart',
+          className: 'Font',
           filter: {
             attributes: {
-              type: 'text',
-              category: 'style',
+              type: 'font',
             },
           },
         },
@@ -70,14 +68,33 @@ module.exports = StyleDictionary.extend({
           destination: 'spacing.dart',
           format: 'flutter/class.dart',
           className: 'Spacing',
+          imports: [
+            'package:flutter/painting.dart',
+          ],
           filter: ({ attributes: { type, subitem } }) => {
             return type.startsWith('spacing') && (type !== 'spacing_squish' || subitem === 'stack')
+          },
+        },
+        {
+          destination: 'asset.dart',
+          format: 'flutter/class.dart',
+          className: 'Asset',
+          imports: [
+            'package:ds/src/types.dart',
+          ],
+          filter: {
+            attributes: {
+              type: 'asset',
+            },
           },
         },
         {
           destination: 'border.dart',
           format: 'flutter/class.dart',
           className: 'Border',
+          imports: [
+            'package:flutter/painting.dart',
+          ],
           filter: {
             attributes: {
               type: 'border',
@@ -88,6 +105,9 @@ module.exports = StyleDictionary.extend({
           destination: 'radius.dart',
           format: 'flutter/class.dart',
           className: 'Radius',
+          imports: [
+            'package:flutter/painting.dart',
+          ],
           filter: {
             attributes: {
               type: 'radius',
@@ -112,6 +132,50 @@ module.exports = StyleDictionary.extend({
             attributes: {
               type: 'shadow',
               subitem: 'stack',
+            },
+          },
+        },
+        {
+          destination: 'typography.dart',
+          format: 'flutter/factory.dart',
+          className: 'Typography',
+          factoryName: 'TextStyle',
+          imports: [
+            'package:flutter/painting.dart',
+          ],
+          filter: {
+            attributes: {
+              type: 'text',
+              category: 'style',
+            },
+          },
+        },
+        {
+          destination: 'button.dart',
+          format: 'flutter/factory.dart',
+          className: 'Button',
+          factoryName: 'ButtonStyleData',
+          imports: [
+            'package:ds/src/types.dart',
+          ],
+          filter: {
+            attributes: {
+              type: 'button',
+              category: 'style',
+            },
+          },
+        },
+        {
+          destination: 'shape.dart',
+          format: 'flutter/factory.dart',
+          className: 'Shape',
+          factoryName: 'ShapeStyleData',
+          imports: [
+            'package:ds/src/types.dart',
+          ],
+          filter: {
+            attributes: {
+              type: 'shape',
             },
           },
         },
