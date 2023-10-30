e51591c6ee11e68bdd90bdaac1515988ee72d1e8
Pedro Silva <9375141+pedrox-hs@users.noreply.github.com>
2023-10-29 23:08:40 -0300

    Use utils in factory template

--- packages/ds/generator/src/utils/template.js
+++ packages/ds/generator/src/utils/template.js
@@ -2,29 +2,46 @@ const fs = require('fs')
 const path = require('path')
 const formatHelpers = require('style-dictionary/lib/common/formatHelpers')
 const _template = require('lodash/template')
+const _camelCase = require('lodash/camelCase')
+const _isPlainObject = require('lodash/isPlainObject')
+
+function formatsFromTemplatesPath (platform, templatesPath) {
+  const templates = fs.readdirSync(templatesPath)
+    .filter((file) => file.endsWith('.template'))
+
+  return templates.reduce((acc, template) => {
+    const name = `${platform}/${template.replace(/\.template$/, '')}`
+    const templatePath = path.join(templatesPath, template)
+
+    return {
+      ...acc,
+      [name]: {
+        formatter: (dictionary, config, file) => {
+          const content = fs.readFileSync(templatePath)
+          const template = _template(content)
+          return template({
+            require,
+            context: { file, config, dictionary, formatHelpers },
+          })
+        },
+      },
+    }
+  }, {})
+}
+
+function formatNewDartObject (classOrFactory, attributes) {
+  const factoryArgs = Object.entries(attributes)
+    .reduce((acc, [name, value]) => {
+      if (_isPlainObject(value)) {
+        value = formatNewDartObject(name, value)
+      }
+      return `${acc}${_camelCase(name)}: ${value},`
+    }, '')
+
+  return `${classOrFactory}(${factoryArgs});`
+}
 
 module.exports = {
-  formatsFromTemplatesPath: (platform, templatesPath) => {
-    const templates = fs.readdirSync(templatesPath)
-      .filter((file) => file.endsWith('.template'))
-
-    return templates.reduce((acc, template) => {
-      const name = `${platform}/${template.replace(/\.template$/, '')}`
-      const templatePath = path.join(templatesPath, template)
-
-      return {
-        ...acc,
-        [name]: {
-          formatter: function (dictionary, config, file) {
-            const content = fs.readFileSync(templatePath)
-            const template = _template(content)
-            return template({
-              require,
-              context: { file, config, dictionary, formatHelpers },
-            })
-          },
-        },
-      }
-    }, {})
-  },
+  formatsFromTemplatesPath,
+  formatNewDartObject,
 }
